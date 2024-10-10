0473e69f5344e8b7d8166c4ae59601ea2de014f9
Soham-Mane <116940634+Soham-Mane@users.noreply.github.com>
2024-10-10 19:11:29 +0530

    Dummy

---
+++ src/components/DummyComponent .js
@@ -0,0 +1,13 @@
+// DummyComponent.js
+import React from 'react';
+
+const DummyComponent = () => {
+    return (
+        <div>
+            <h1>Hello, this is a dummy component!</h1>
+            <p>This is just a simple component for learning purposes.</p>
+        </div>
+    );
+}
+
+export default DummyComponent;
